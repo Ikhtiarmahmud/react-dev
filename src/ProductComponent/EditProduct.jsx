63d9fe3835021c19234898fcf66d34c4a81f939c
ikhtiaruddin <ikhtiar.uddin@bs-23.net>
2021-03-07 22:01:58 +0600

    Update product through api

--- src/ProductComponent/EditProduct.jsx
+++ src/ProductComponent/EditProduct.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const ProductCreate = () => {
+const ProductEdit = () => {
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState('');
+    const [loading, setLoading] = useState(true);
     const params = useParams();
 
     useEffect(() => {
@@ -15,13 +16,23 @@ const ProductCreate = () => {
             setPrice(res.data.price);
             setDescription(res.data.description);
             setCategory(res.data.category);
+            setLoading(false);
         }).catch(err => err.response);
     },[])
 
+    const updateHandler = () => {
+        axios.put(`https://fakestoreapi.com/products/${params.id}`, {
+            title, price, description, category
+        }).then(res => {
+            console.log('Product Updated Successfully!', res.data)
+        }).catch(err => console.log(err.response))
+    }
+
     return (
-        <>
+        <> 
             <div style={card}>
                 <h1>Edit Product !</h1>
+                { loading && <p>Loading . . .</p> }
 
                 <div style={mt1}>
                     <label>Title</label>
@@ -48,7 +59,7 @@ const ProductCreate = () => {
                     }}/>
                 </div>
                 <div>
-                    <button>Update</button>
+                    <button onClick={updateHandler}>Update</button>
                 </div>
             </div>
         </>
@@ -68,4 +79,4 @@ const mr = {
     marginLeft: '30px',
 }
 
-export default ProductCreate;
+export default ProductEdit;
